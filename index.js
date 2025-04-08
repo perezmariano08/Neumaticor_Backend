5fcc5c10dcdd61b533a72239c8f4f758c7ab20f9
Mariano Perez <perezmariano.pm@gmail.com>
2025-04-08 17:05:40 -0300

    cambios en cors

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ app.use(express.json());
 app.use(cors({
     origin: [
         'http://localhost:5173', 
-        'http://localhost:5174'
+        'http://localhost:5174',
+        'https://neumaticor.vercel.app'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
