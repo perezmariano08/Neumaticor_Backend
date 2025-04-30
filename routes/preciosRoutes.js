b67258be5b6fed16aad6aabd268b87c4e5aad219
Mariano Perez <perezmariano.pm@gmail.com>
2025-04-30 17:59:27 -0300

    cambios se quito revisarAdmin

--- routes/preciosRoutes.js
+++ routes/preciosRoutes.js
@@ -6,7 +6,7 @@ const { revisarAdmin, verificarToken } = require('../middlewares/auth');
 
 // Esta es la ruta correcta según tu código
 router.get('/productos/:id?', productosController.getProductosConPrecio);
-router.get('/listas', revisarAdmin, preciosController.getListasPrecios);
-router.post('/aplicar-porcentaje', revisarAdmin, preciosController.aplicarPorcentaje);
+router.get('/listas', preciosController.getListasPrecios);
+router.post('/aplicar-porcentaje', preciosController.aplicarPorcentaje);
 
 module.exports = router;
--- routes/usuariosRoutes.js
+++ routes/usuariosRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const usuariosController = require('../controllers/usuariosController');
-const { revisarAdmin, verificarToken } = require('../middlewares/auth');
+const { revisarAdmin } = require('../middlewares/auth');
 
 // Esta es la ruta correcta según tu código
-router.get('/roles',  revisarAdmin, usuariosController.getRoles);
-router.get('/solicitudes',  revisarAdmin, usuariosController.getSolicitudes);
-router.get('/:id?', revisarAdmin, usuariosController.getUsuarios);
+router.get('/roles', usuariosController.getRoles);
+router.get('/solicitudes', usuariosController.getSolicitudes);
+router.get('/:id?', usuariosController.getUsuarios);
 
 module.exports = router;
