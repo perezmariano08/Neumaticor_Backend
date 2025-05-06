5080b184a7c7c814084cf81cce852234c71c9cb0
Mariano Perez <perezmariano.pm@gmail.com>
2025-05-06 12:12:07 -0300

    cambios en cors origin

--- routes/pedidosRoutes.js
+++ routes/pedidosRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const pedidosController = require('../controllers/pedidosController');
 const { verificarToken, revisarAdmin } = require('../middlewares/auth');
 
-// Esta es la ruta correcta según tu código
+// POST
 router.post('/finalizar-pedido', pedidosController.finalizarPedido);
+// PUT
 router.put('/actualizar-pedido', verificarToken, revisarAdmin, pedidosController.actualizarPedido);
+// GET
 router.get('/usuarios/:id_usuario', pedidosController.getPedidosPorUsuario);
 router.get('/usuarios/detalle/:id_usuario', pedidosController.getPedidosDetallePorUsuario);
 router.get('/:id?', pedidosController.getPedidos);
